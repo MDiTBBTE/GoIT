ecf8bcddf1cdfba105bfb2e9e1f2196b057cdad9
Andrii Korniienko <andrii.ds.kor@gmail.com>
2019-08-01 20:26:34 +0300

    change in structure

--- js/13homeTask/src/index.js
+++
@@ -1,9 +0,0 @@
-import './css/styles.css';
-import View from './js/view';
-import Module from './js/module';
-import Controller from './js/controller';
-
-const view = new View();
-const module = new Module();
-
-new Controller(module, view);
